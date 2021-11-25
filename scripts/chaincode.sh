987a32c62bacd844118c09459160ee6ef7225426
MicroMetaverse/元小宇 <zhaijunfeng2012@hotmail.com>
2021-11-25 19:50:32 +0800

    ls ${LOCAL_CHAINCODE_PATH}

--- scripts/chaincode.sh
+++ scripts/chaincode.sh
@@ -16,9 +16,9 @@ go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 echo "set go goproxy,end"
 go get -d ${CCURL}
-echo "---ll---"
-ll ${LOCAL_CHAINCODE_PATH}
-echo "---ll---"
+echo "---ls---"
+ls ${LOCAL_CHAINCODE_PATH}
+echo "---ls---"
 echo "---pwd---"
 pwd
 echo "---pwd---"
