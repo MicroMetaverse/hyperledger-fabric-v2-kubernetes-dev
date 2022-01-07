722379be2e884009e580a81cd39b55f5c7528e09
Administrator <zhaijunfeng2012@gmail.com>
2022-01-07 15:16:48 +0800

    721

--- hlf-erc721.sh
+++ hlf-erc721.sh
@@ -91,15 +91,15 @@ ccCommit)
 	;;
 
 ccInvoke)
-  CTOR=$1
-  echo "ccInvoke:${CTOR}"
+  CTOR=$2
+  echo "---ccInvoke:${CTOR}"
 	invoke ${CCNAME} ${CHANNEL_ID} ${CTOR} | sh -c "kubectl --namespace org1 exec -i $(kubectl -n org1 get pod -l app=admin -o name) -- sh -"
 	;;
 
 
 ccQuery)
-  CTOR=$1
-  echo "ccQuery:${CTOR}"
+  CTOR=$2
+  echo "---ccQuery:${CTOR}"
 	for ORG in org1 org2 org3; do
 		for PEER in peer0; do
 			echo "Quering on ${PEER}.${ORG}"
